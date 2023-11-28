6099928be0123466ec6f2d41e5a7de2e358e0c12
DaoSeckar <57205859+DaoSeckar@users.noreply.github.com>
2023-11-28 14:35:06 -0800

    project change to flask

--- project.py
+++ project.py
@@ -1,30 +1,15 @@
-import tkinter as tk
-from tkinter import ttk
+# import the Flask class from the flask module
+from flask import Flask
 
-def open_second_gui():
-    second_gui = tk.Toplevel(root)
-    second_gui.title("Second GUI")
-    second_gui.geometry("400x300")
+# create a Flask object called app
+app = Flask(__name__)
 
-    label = ttk.Label(second_gui, text="This is the second GUI.")
-    label.pack(padx=20, pady=20)
+# define a route to the home page
+# create a hey_there function
+@app.route("/")
+def hey_there():
+    return "Hey there, welcome to my web application!"
 
-# Create the main GUI
-root = tk.Tk()
-root.title("Main GUI")
-
-# Create a frame to hold the map
-map_frame = ttk.Frame(root, width=400, height=300, relief="solid", borderwidth=2)
-map_frame.grid(row=0, column=0, padx=10, pady=10)
-
-# Create buttons at the bottom
-button_frame = ttk.Frame(root)
-button_frame.grid(row=1, column=0, pady=10)
-
-open_button = ttk.Button(button_frame, text="Open Second GUI", command=open_second_gui)
-open_button.grid(row=0, column=0, padx=5)
-
-exit_button = ttk.Button(button_frame, text="Exit", command=root.destroy)
-exit_button.grid(row=0, column=1, padx=5)
-
-root.mainloop()
+@app.route("/about")
+def about_page():
+    return "About Page"
