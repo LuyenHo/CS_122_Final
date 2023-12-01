64d045e6780b83c349e9559f90081a22f2841aa1
“Luyen <ngocluyen.ho@sjsu.edu>
2023-12-01 12:46:38 -0800

    generate file change

--- generate_chart.py
+++ generate_chart.py
@@ -7,20 +7,25 @@ import pandas as pd
 def generate_chart():
     # Read the CSV file
     df = pd.read_csv('static/California_Fire_Incidents.csv')
+    acres_sum = df.groupby(by='ArchiveYear').AcresBurned.sum()
 
     # plot Acres Burned vs Year
-    fig, ax = plt.subplots(figsize=(16, 4))
-    acres_sum = df.groupby(by='ArchiveYear').AcresBurned.sum()
-    ax.plot(acres_sum.index, acres_sum, color='red')
-    ax.set_xlabel('Archive Year')
-    # plt.xlabel('Archive Year')
-    ax.set_ylabel('Acres Burned')
+    # fig, ax = plt.subplots(figsize=(16, 4))
+    # ax.plot(acres_sum.index, acres_sum, color='red')
+    # ax.set_xlabel('Archive Year')
+    # ax.set_ylabel('Acres Burned')
     # plt.ylabel('AcresBurned')
-    ax.grid(True)
-    ax.set_title('Wildfire from 2013 to 2019')
-    # plt.grid()
+    # ax.grid(True)
+    # ax.set_title('Wildfire from 2013 to 2019')
+    # image_path = 'static/chart.png'
+    # fig.savefig(image_path)
+
+    #window
+    plt.figure(figsize=(16, 4))
+    plt.xlabel('Archive Year')
+    plt.title('Wildfire from 2013 to 2019')
+    plt.grid()
     plt.show()
-    image_path = 'static/chart.png'
-    fig.savefig(image_path)
+    
     plt.close()
-    return image_path
+    # return image_path
